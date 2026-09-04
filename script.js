77947317ec2145a8e61b46e24f84214253a9522b
guiandumagat18-prog <guiandumagat18@gmail.com>
2026-09-04 17:29:11 +0800

    addEventListener

--- script.js
+++ script.js
@@ -24,13 +24,33 @@ console.log(greet("Mayung aga ", ["Guian" , "Mark" , "Arjay" , "Myco."]));
 
 const heading = document.querySelector("h1");
 heading.textContent = "Garson Dumagat Sr.";
-heading.style.color = "blue";
-heading.style.backgroundColor = "lightgreen";
+heading.style.color = "purple";
+heading.style.backgroundColor = "yellow";
 heading.style.fontSize = "60px";
 console.log(heading.textContent);
+
+heading.addEventListener("click", function() {
+    heading.style.color = "red";
+    console.log("Heading was clicked.");
+});
 
 const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading);
 
 const projectHeading = document.querySelector("#services h2");
 console.log(projectHeading);
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function() {
+    isOff = !isOff;
+    if(isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+});
