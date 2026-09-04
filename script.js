d3a16189957a47b098f41eee1b5d2710272cdf46
guiandumagat18-prog <guiandumagat18@gmail.com>
2026-09-04 16:32:41 +0800

    querySelector

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ function greet(greetings,names = []) {
     }
 }
 console.log(greet("Mayung aga ", ["Guian" , "Mark" , "Arjay" , "Myco."]));
+
+const heading = document.querySelector("h1");
+console.log(heading);
+
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading);
+
+const projectHeading = document.querySelector("#services h2");
+console.log(projectHeading);
