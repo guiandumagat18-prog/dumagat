52caee4ed8393aa285466994666f756375822608
guiandumagat18-prog <guiandumagat18@gmail.com>
2026-09-04 14:39:25 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log("Hello, world!");
 console.log("From script.js file.");
+
+const myName = "Guian Mark Dumagat";
+let age = 21;
+const numbers = "09666734556";
+const address = "Brgy. XXI, Palanca Village, Victorias City.";
+
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${numbers}`);
+console.log(`Address: ${address}`);
+console.log(67);
